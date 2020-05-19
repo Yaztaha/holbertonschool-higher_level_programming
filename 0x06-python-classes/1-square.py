6bb13b69a95a4cfd4d19c0a57c280f227418dedd
Taha Yazidi <yazi.taha@gmail.com>
2020-05-19 22:07:32 +0000

    squareClass

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Square class that defines a square by:
+Private instance attribute: size
+Instantiation with size (no type/value verification)
+You are not allowed to import any module
+"""
+class Square:
+"""
+Square class
+
+"""
+    def __init__(self, size):
+        self.__size = size
