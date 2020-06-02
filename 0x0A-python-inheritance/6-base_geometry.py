3925d3e1d52cef5adf1246818acade2ea3322b4d
Taha Yazidi <yazi.taha@gmail.com>
2020-06-02 17:44:16 +0000

    base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Base geometry module """
+
+
+class BaseGeometry:
+    """ Base geometry class """
+
+    def area(self):
+        """ public instance method """
+        raise Exception("area() is not implemented")
