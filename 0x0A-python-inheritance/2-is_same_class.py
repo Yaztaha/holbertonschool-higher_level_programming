ad08f43e3676024c3757e987201faeef37bf9023
Taha Yazidi <yazi.taha@gmail.com>
2020-06-02 17:23:57 +0000

    is_same_class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Same class's module """
+
+
+def is_same_class(obj, a_class):
+    """ Function that checks if the object is an instance """
+
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
