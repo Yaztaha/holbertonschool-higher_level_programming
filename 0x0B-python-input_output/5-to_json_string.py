331dc784b27a5556fded9cb4333ae269f0acf1cd
Taha Yazidi <yazi.taha@gmail.com>
2020-06-03 17:22:51 +0000

    to_json_string

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -2,6 +2,7 @@
 """ JSON representation of a string """
 import json
 
+
 def to_json_string(my_obj):
     """ Function that return JSON representation of a string """
     return json.dumps(my_obj)
