e168bf1de8fc8dc480b5cd6957fa8e3de95fe45e
Taha Yazidi <yazi.taha@gmail.com>
2020-06-03 17:14:00 +0000

    read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Read file """
+
+
+def read_file(filename=""):
+    """ Function that reads a file's content """
+    with open(filename, encoding="UTF-8") as myFile:
+
+        print(myFile.read())
