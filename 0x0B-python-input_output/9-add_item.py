cad87fe29d22ca37900a63dc9a33ce4ac2af38e0
Taha Yazidi <yazi.taha@gmail.com>
2020-06-04 11:55:04 +0000

    add_item

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """ Script that adds all arguements to a Python list, ans then save them to a file """
-import json
+
 import sys
+
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 py_list = []
 
 try:
-    py_list = load_from_json_file("add_item_json")
+    py_list = load_from_json_file("add_item.json")
     save_to_json_file(py_list + sys.argv[1:], "add_item.json")
 except:
     save_to_json_file(py_list + sys.argv[1:], "add_item.json")
