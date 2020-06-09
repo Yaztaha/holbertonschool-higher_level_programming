4c76c9ab8c44f0bc9cbbe2a8966fcc460f6b0620
Taha Yazidi <yazi.taha@gmail.com>
2020-06-09 19:44:19 +0000

    Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Base module """
+
+
+class Base:
+    """ Base class """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initializing method """
+
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
