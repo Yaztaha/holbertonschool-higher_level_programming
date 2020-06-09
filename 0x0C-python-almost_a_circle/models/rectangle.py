f2df0ce0e65f37595ca224ba357a8f2e35da52b8
Taha Yazidi <yazi.taha@gmail.com>
2020-06-09 19:55:40 +0000

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Rectangle module """
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Revctangle class """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Rectangle initialization method """
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        
