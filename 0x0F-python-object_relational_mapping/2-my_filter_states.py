58426c6624626c95035a01bb917821d34c137563
Taha Yazidi <yazi.taha@gmail.com>
2020-08-13 21:02:33 +0000

    filter_st

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     c = db.cursor()
     q = "SELECT id, name FROM states\
     WHERE name = (%s) ORDER BY states.id".format(argv[4])
