32aff5d675e65059c72de12e1107cef05887ab53
Taha Yazidi <yazi.taha@gmail.com>
2020-08-13 21:10:03 +0000

    filter_st

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,14 +5,17 @@ of hbtn_0e_0_usa where name matches the argument """
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    c = db.cursor()
-    q = "SELECT id, name FROM states\
-    WHERE name = (%s) ORDER BY states.id ASC".format(argv[4])
-    c.execute(q, [argv[4]])
-    states_rows = c.fetchall()
+import MySQLdb
+import sys
 
-    for i in range(len(states_rows)):
-        print("{}".format(states_rows[i]))
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE \
+    states.name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
