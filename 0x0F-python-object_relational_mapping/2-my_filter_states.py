0939dfa0d312fd88e4ba00ddb5f09c809b0a6ce3
Taha Yazidi <yazi.taha@gmail.com>
2020-08-13 17:23:54 +0000

    filter_states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Script that takes in an argument and displays all values in
+the states table
+of hbtn_0e_0_usa where name matches the argument """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    q = "SELECT id, name FROM states WHERE name = (%s) ORDER BY id"
+    c.execute(q, [argv[4]])
+    states_rows = c.fetchall()
+
+    for i in range(len(states_rows)):
+        print("{}".format(states_rows[i]))
