13b754f0daf65d523258c9e09f0ec0a281aadd88
Taha Yazidi <yazi.taha@gmail.com>
2020-12-17 13:22:33 +0100

    lastPT1/2

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Adds a new city object to the database"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format
+                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for city, state in session.query(
+            City, State).filter(
+            City.state_id == State.id).all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
+    session.close()
