919172bd5a2aeaf7c9884c7cedcdf22836189b25
Taha Yazidi <yazi.taha@gmail.com>
2020-12-18 14:22:15 +0100

    peak_func

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function that finds a peak in a list of unsorted integers with the minimal time complexity"""
+"""Function that finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
