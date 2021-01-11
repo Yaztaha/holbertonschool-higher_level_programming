30f62333c0336da9300e698f3d0c20810a649bb4
yaztaha <yazi.taha@gmail.com>
2021-01-11 11:47:01 +0000

    error_urllib.error.HTTPError

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ Request URL with error exception management """
-import urllib.request
+import urllib
 import sys
-
-req = urllib.request.Request(sys.argv[1])
-try:
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print(html.decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print('Error Code: ', e.code)
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print('Error Code: ', e.code)
