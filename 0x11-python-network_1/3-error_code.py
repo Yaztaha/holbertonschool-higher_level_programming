d3a6c3ec9203439abfc86bfc6868797926800610
yaztaha <yazi.taha@gmail.com>
2021-01-11 12:04:09 +0000

    HTTP_error_handling

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Request URL with error exception management """
 import urllib.request
+import urllib.error
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    req = urllib.request.Request(sys.argv[1])
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            html = response.read()
-            print(html.decode('utf-8'))
-    except urllib.error.HTTPError as e:
-        print('Error Code: ', e.code)
+            with urllib.request.urlopen(req) as response:
+                    print(response.read().decode("utf-8"))
+    except urllib.error.URLError as e:
+            print("Error code: {}".format(e.code))
