f4241ad83582b9d64641b3273d58e77886fa7572
yaztaha <yazi.taha@gmail.com>
2021-01-11 11:15:35 +0000

    status_page

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         html = res.read()
         print("Body response:\n\
-\t- type:{}\n\
-\t- content:{}\n\
-\t- utf8 content:{}".format(type(html), html, html.decode('utf-8')))
+\t- type: {}\n\
+\t- content: {}\n\
+\t- utf8 content: {}".format(type(html), html, html.decode('utf-8')))
