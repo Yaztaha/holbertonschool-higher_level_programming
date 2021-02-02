34a7f995b6269663231fcab1349d4a1c9c8e2b15
yaztaha <yazi.taha@gmail.com>
2021-02-02 17:52:32 +0000

    sentence_multiplier

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,5 +3,5 @@ const myVar = 'C is fun';
 const coeficient = process.argv[2];
 
 if (coeficient) {
-  for (let i = 0; i < times; i++) console.log(myVar);
+  for (let i = 0; i < coeficient; i++) console.log(myVar);
 } else console.log('Missing number of occurrences');
