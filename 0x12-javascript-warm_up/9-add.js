42aedbaf8021f1c6d3ec81a9442a837bd7f1e0ec
yaztaha <yazi.taha@gmail.com>
2021-02-02 17:57:33 +0000

    add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+function add (a, b) {
+  return (a + b);
+}
+const first = parseInt(process.argv[2]);
+const second = parseInt(process.argv[3]);
+const result = add(first, second);
+console.log(result);
