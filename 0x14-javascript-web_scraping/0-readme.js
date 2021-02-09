db1b6700975b225c686e41e3e4e14b8c5e2654bb
yaztaha <yazi.taha@gmail.com>
2021-02-09 21:12:56 +0000

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const fp = require('fp');
-const filetoread = process.argv[2];
-fp.readFile(filetoread, 'utf-8', function (err, data) {
-  if (err) { return console.log(err); } else { console.log(data.trim()); }
-});
