82a75646e5102771d588f76e3fd9fccca8b34739
yaztaha <yazi.taha@gmail.com>
2021-02-09 21:33:29 +0000

    add

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
